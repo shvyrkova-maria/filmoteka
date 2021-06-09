e4ee4d281f128af03cc73e3e19fe46482d30ed2e
Maksim Tsopa <maks98242@gmail.com>
2021-06-09 16:39:00 +0300

    create header navigation

--- src/js/headerNav.js
+++ src/js/headerNav.js
@@ -1,17 +1,28 @@
-
-
 const refs = {
   headerHomeBtn: document.querySelector('#headerHomeBtn'),
   headerLibraryBtn: document.querySelector('#headerLibraryBtn'),
+  headerHomeContent: document.querySelector('.header__home-content'),
+  headerLibraryContent: document.querySelector('.header__library-content'),
 };
 
 console.log(refs.headerHomeBtn);
-
 console.log(refs.headerLibraryBtn);
 
-refs.headerHomeBtn.addEventListener('click', changeHeader);
+refs.headerHomeBtn.addEventListener('click', changeHeaderHome);
+refs.headerLibraryBtn.addEventListener('click', changeHeaderLibrary);
 
+function changeHeaderHome() {
+  refs.headerHomeBtn.classList.add('current_page');
+  refs.headerLibraryBtn.classList.remove('current_page');
 
-function changeHeader() {
-    console.log('+')
+  refs.headerHomeContent.classList.remove('header__content-hide');
+  refs.headerLibraryContent.classList.add('header__content-hide');
+}
+
+function changeHeaderLibrary() {
+  refs.headerHomeBtn.classList.remove('current_page');
+  refs.headerLibraryBtn.classList.add('current_page');
+
+  refs.headerLibraryContent.classList.remove('header__content-hide');
+  refs.headerHomeContent.classList.add('header__content-hide');
 }
