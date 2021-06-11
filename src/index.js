6d525ab6a08c885a3a977788a95dcacbbbc8db0b
Maksim Tsopa <maks98242@gmail.com>
2021-06-11 22:25:30 +0300

    merge footer

--- src/index.js
+++ src/index.js
@@ -2,23 +2,8 @@ import './sass/main.scss';
 import 'modern-normalize/modern-normalize.css';
 import './js/modal';
 import './js/headerNav';
-<<<<<<< Updated upstream
 import './js/spinner';
 import './js/apiService';
 import './js/gallery';
 import './js/btnUp';
 import './js/getLocalStorage';
-=======
-import './js/our-team';
-
-
-
-
-
-
-
-
-
-
-// import debounce from 'lodash.debounce'; Импортировать дебаунс таким ообразом!!!!!
->>>>>>> Stashed changes
