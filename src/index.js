00d00a2e5b53dd2cbe2a72a1a688cc116b423cba
yuliiasonko <yuliiasonko@gmail.com>
2021-06-11 21:03:31 +0300

    Добавила футер

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import './sass/main.scss';
 import 'modern-normalize/modern-normalize.css';
 import './js/modal';
 import './js/headerNav';
+<<<<<<< Updated upstream
 import './js/spinner';
 import './js/apiService';
 import './js/gallery';
 import './js/btnUp';
 import './js/getLocalStorage';
+=======
+import './js/our-team';
+
+
+
+
+
+
+
+
+
+
+// import debounce from 'lodash.debounce'; Импортировать дебаунс таким ообразом!!!!!
+>>>>>>> Stashed changes
